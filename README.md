ae7f46b3ddd4a8c6ac44f5376c0c04832d1a53d8
neex <neex-io@users.noreply.github.com>
2017-07-01 18:43:43 -0700

    Add error messages for empty fields.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Your Task
 1. If the user's form submission is not valid, you should reject it and re-render the form with some feedback to inform the user of what they did wrong. The following things should trigger an error:
-    * The user leaves any of the following fields empty: username, password, verify password.
+    *[x] The user leaves any of the following fields empty: username, password, verify password.
     * The user's username or password is not valid -- for example, it contains a space character or it consists of less than 3 characters or more than 20 characters (e.g., a username or password of "me" would be invalid).
     * The user's password and password-confirmation do not match.
     * The user provides an email, but it's not a valid email. Note: the email field may be left empty, but if there is content in it, then it must be validated. The criteria for a valid email address in this assignment are that it has a single @, a single ., contains no spaces, and is between 3 and 20 characters long.
--- app.py
+++ app.py
@@ -12,9 +12,30 @@ def display_signup():
     return render_template('signup_form.html')
 
 
+def is_empty(str):
+    if str == "":
+        return True
+    else:
+        return False
+
 @app.route("/signup", methods=["POST"])
-def signup_user():
-    return "THIS IS POST"
+def validate_signup_form():
+    username = request.form["username"]
+    password = request.form["password"]
+    password_repeat = request.form["password_repeat"]
+    email = request.form["email"]
+    
+    if is_empty(username) and (is_empty(password) or is_empty(password_repeat)):
+        return render_template('signup_form.html', 
+                username_error="User field cannot be left empty.", 
+                password_error="Password fields cannot be left empty.")
+    elif is_empty(username):
+        return render_template('signup_form.html', username_error="User field cannot be left empty.")
+    elif is_empty(password) or is_empty(password_repeat):  
+        return render_template('signup_form.html', password_error="Password fields cannot be left empty.")
+    else: 
+        return render_template("signup_form.html", welcome="all good!")
+
 
 if __name__ == "__main__":
     app.run()
