68601f96d76a050c0de111360b0db703782f046c
neex <neex-io@users.noreply.github.com>
2017-07-02 19:37:02 -0700

    Add server-side data validation.

--- app.py
+++ app.py
@@ -1,5 +1,44 @@
 from flask import Flask, request, redirect, render_template
 
+# functions required for validating signup_form input
+def is_empty(str):
+    if str == "":
+        return True
+    else:
+        return False
+
+def valid_username():
+    pass
+
+def valid_password(password):
+    if " " in password:
+        return False
+    if len(password) < 3 or len(password) > 20:
+        return False
+    return True
+
+def password_match(password, password_repeat):
+    return password == password_repeat
+
+def over_one_dot(str):
+    str_list = str.split(".")
+    if len(str_list) > 2:
+        return True
+
+def valid_email(email):
+    if email != "":
+        if " " in email:
+            return False
+        if len(email) < 3 or len(email) > 20:
+            return False
+        if "@" not in email:
+            return False
+        if over_one_dot(email):
+            return False
+    # no email is required thus empty string is a valid input
+    return True
+
+
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
@@ -10,13 +49,6 @@ def index():
 @app.route("/signup")
 def display_signup():
     return render_template('signup_form.html')
-
-
-def is_empty(str):
-    if str == "":
-        return True
-    else:
-        return False
 
 @app.route("/signup", methods=["POST"])
 def validate_signup_form():
@@ -30,10 +62,16 @@ def validate_signup_form():
     password_error = "Password field cannot be left empty." if is_empty(password) else ""
     password_repeat_error = "Password field cannot be left empty." if is_empty(password_repeat) else ""
 
+    # Validate data
+    password_error = "" if valid_password(password) else "Password must be between 3 and 20 characters. It can only contain XYZ."
+    password_repeat_error = "" if password_match(password, password_match) else "Passwords do not match"
+    email_error = "" if valid_email(email) else "This is not a valid email"
+
     return render_template('signup_form.html', 
             username_error=username_error, 
             password_error=password_error, 
-            password_repeat_error=password_repeat_error)
+            password_repeat_error=password_repeat_error, 
+            email_error=email_error)
 
 
 
